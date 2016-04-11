f72c36b678b3690529cb4590536cebdf025b3067
Zach <zachary.whitten.11@cnu.edu>
2016-04-11 16:01:20 -0400

    Moving files round 2

---
+++ Now Playing in iTunes/ApplescriptBridge.h
@@ -0,0 +1,18 @@
+//
+//  ApplescriptBridge.h
+//  Now Playing in iTunes
+//
+//  Created by Zachary Whitten on 4/11/16.
+//  Copyright © 2016 WCNURadio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MyStuff.h"
+
+@interface ApplescriptBridge : NSObject
+
+@property id<myStuffProtocol> myInstance;
+
+-(Boolean)isiTunesPlaying;
+
+@end
